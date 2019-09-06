028f44e7541cb42d96130bda56fb72f2f7964b13
mangonob <mangonob@qq.com>
2019-09-06 19:33:17 +0800

    M Change skins

--- Demo/GameViewController.swift
+++ Demo/GameViewController.swift
@@ -11,6 +11,8 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
+    private var isWalked: Bool = true
+    private var isMaleGoblins: Bool = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,8 +32,31 @@ class GameViewController: UIViewController {
         
         let skeleton = DZSpineSceneBuilder.loadSkeletonName("spineboy", scale: 1)!
         let builder = DZSpineSceneBuilder.builder() as! DZSpineSceneBuilder
-        let node = builder.node(with: skeleton, animationNames: ["walk", "jump", "walk"], loop: true)!
-        spSkeleton_setSkinByName(skeleton.spineContext.pointee.skeleton, "default")
+        let node = builder.node(with: skeleton, animationName: isWalked ? "walk" : "jump", loop: true)!
+        
+        let placeholder = SKNode()
+        placeholder.position = CGPoint(x: size.width / 2, y: size.height / 3)
+        placeholder.addChild(node)
+        scene.addChild(placeholder)
+        return scene
+    }
+    
+    func buildGoblin(size: CGSize) -> SKScene {
+        let scene = SKScene(size: size)
+        scene.scaleMode = .aspectFill
+        scene.backgroundColor = .white
+
+        let skeleton = DZSpineSceneBuilder.loadSkeletonName("goblins", scale: 1)!
+        
+        if isMaleGoblins {
+            spSkeleton_setSkinByName(skeleton.spineContext.pointee.skeleton, "goblin")
+        } else {
+            spSkeleton_setSkinByName(skeleton.spineContext.pointee.skeleton, "goblingirl")
+        }
+        
+        spSkeleton_setSlotsToSetupPose(skeleton.spineContext.pointee.skeleton)
+        let builder = DZSpineSceneBuilder.builder() as! DZSpineSceneBuilder
+        let node = builder.node(with: skeleton, animationName: "walk", loop: true)!
 
         let placeholder = SKNode()
         placeholder.position = CGPoint(x: size.width / 2, y: size.height / 3)
@@ -54,5 +79,23 @@ class GameViewController: UIViewController {
 
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+    @IBAction func jumpAction(_ sender: Any) {
+        isWalked = !isWalked
+        
+        if let view = self.view as! SKView? {
+            view.presentScene(buildSpineboyLoop(size: view.bounds.size))
+        }
+        
+        (sender as? UIButton)?.setTitle(isWalked ? "Jump" : "Walk", for: .normal)
+    }
+    
+    @IBAction func changeSkin(_ sender: Any) {
+        isMaleGoblins = !isMaleGoblins
+
+        if let view = self.view as! SKView? {
+            view.presentScene(buildGoblin(size: view.bounds.size))
+        }
     }
 }
